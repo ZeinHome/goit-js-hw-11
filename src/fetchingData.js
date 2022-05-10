e71cc05a88bc85c4406c426181b72aee56043b0e
ZeinHome <zein3288@gmail.com>
2022-05-10 16:42:34 +0300

    js

---
+++ src/fetchingData.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+export default class NewsApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    async fetchData() {
+        const params = {
+            key: '27200028-8fc69fbb3d566c8420050baaa',
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+        }
+
+        return await axios.get(`https://pixabay.com/api/?key=27200028-8fc69fbb3d566c8420050baaa&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+            .then(object => {
+                this.pageIncrement();
+                return {
+                    totalHits: object.data.totalHits,
+                    hits: object.data.hits,
+                };
+            })
+            .catch(error => console.log('error', error));
+    }
+    resetpage() {
+        this.page = 1;
+    }
+    pageIncrement() {
+        this.page += 1;
+    }
+    nowPage() {
+        return this.page - 1;
+    }
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
